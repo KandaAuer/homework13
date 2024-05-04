53b3ea4afd310acbf43d1f901b6731089077c44a
ktpx6 <ktpx6@bk.ru>
2024-05-04 03:43:08 +0300

    hw 13

---
+++ src/Author.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String toString() {
+        return "Автор: " + this.firstName + " " + this.lastName;
+    }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) &&
+                Objects.equals(lastName, author.lastName);
+    }
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Анджей", " Сапковский");
+        Author author2 = new Author("Агата", " Кристи");
 
-        System.out.println("Hello world!");
+        Book book1 = new Book("Ведьмак. Час Презрения", author1, 1995);
+        Book book2 = new Book("Убийство в 'Восточном экспрессе'", author2, 1934);
+
+        book1.setPublicationYear(1997);
+
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
